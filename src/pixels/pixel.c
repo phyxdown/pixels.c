691b4981516d5a91aa617974aa1bceefac964c4d
phyxdown <wucc@Ctrip.com>
2015-07-02 11:38:27 +0800

    modify api

--- src/pixels/pixel.c
+++ src/pixels/pixel.c
@@ -1,36 +1,41 @@
 #include <pixels/pixel.h>
 #include <string.h>
 PIXELS_API STATUS copy_pixel(PIXEL src, PIXEL dest, const MAP map){
-	int len = strlen(map);
-	for (int i = 0 ; i < len; i++) {
+	PIXELSIZE size = pixel_size(map);
+	for (int i = 0 ; i < size; i++) {
 		*(src+i) = *(dest+i);
 	}
 }
 
+PIXELS_API PIXELSIZE pixel_size(const MAP map){
+	return strlen(map);
+}
 
-PIXELS_API STATUS scramble_pixels(PIXELS origin, PIXELS dest, LEN len, int a, int b){
+
+PIXELS_API STATUS scramble_pixels(PIXELS origin, PIXELS dest, const MAP map, LEN len, int a, int b){
 	PIXEL origin_marker = origin;
 	PIXEL dest_marker = dest;
+	PIXELSIZE size = pixel_size(map);
 	for (int i = 0; i < len*len; i++) {
 		int x = i%len;
 		int y = i/len;
 		int s_x = mod(  x+      b*y, len);
 		int s_y = mod(a*x+(a*b+1)*y, len);
 		int s_i = s_y*len+s_x;
-		copy_pixel(dest_marker+s_i*3, origin_marker+i*3, "RGB");
+		copy_pixel(dest_marker+s_i*size, origin_marker+i*size, map);
 	}
 }
 
-PIXELS_API STATUS rescramble_pixels(PIXELS origin, PIXELS dest, LEN len, int a, int b){
+PIXELS_API STATUS rescramble_pixels(PIXELS origin, PIXELS dest, const MAP map, LEN len, int a, int b){
 	PIXEL origin_marker = origin;
 	PIXEL dest_marker = dest;
+	PIXELSIZE size = pixel_size(map);
 	for (int i = 0; i < len*len; i++) {
 		int x = i%len;
 		int y = i/len;
 		int r_x = mod((a*b+1)*x-b*y, len);
 		int r_y = mod(     -a*x+  y, len);
 		int r_i = r_y*len+r_x;
-		copy_pixel(dest_marker+r_i*3, origin_marker+i*3, "RGB");
+		copy_pixel(dest_marker+r_i*size, origin_marker+i*size, map);
 	}
 }
-
--- src/pixels/pixel.h
+++ src/pixels/pixel.h
@@ -3,10 +3,11 @@
 #include <pixels/math.h>
 typedef unsigned char* PIXELS;
 typedef unsigned char* PIXEL;
+typedef int PIXELSIZE;
 typedef unsigned long LEN; 
 typedef char* MAP;
 
 PIXELS_API STATUS copy_pixel(PIXEL src, PIXEL dest, const MAP map);
-PIXELS_API STATUS scramble_pixels(PIXELS origin, PIXELS dest, LEN len, int a, int b);
-PIXELS_API STATUS rescramble_pixels(PIXELS origin, PIXELS dest, LEN len, int a, int b);
-
+PIXELS_API PIXELSIZE pixel_size(const MAP map);
+PIXELS_API STATUS scramble_pixels(PIXELS origin, PIXELS dest, const MAP map, LEN len, int a, int b);
+PIXELS_API STATUS rescramble_pixels(PIXELS origin, PIXELS dest, const MAP map, LEN len, int a, int b);
