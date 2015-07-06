1b1e66080573331fb7470262ad573b4e76d93f0b
phyxdown <wucc@Ctrip.com>
2015-07-06 18:00:36 +0800

    add print methods, idct does'nt work

--- src/pixels/pixel.c
+++ src/pixels/pixel.c
@@ -5,6 +5,32 @@
 
 const double PI = 3.1415926535898f;
 
+void print_mp(const PIXELS pixels, LEN len){
+	PIXEL p = &*pixels;
+	for(int i =0 ; i < len; i++){
+		for(int j =0 ; j < len; j++){
+			unsigned char* bp = (unsigned char*)p;
+			printf("(%d,%d,%d,%d) ", *bp, *(bp+1), *(bp+2), *(bp+3));
+			bp = NULL;
+			p++;
+		}
+		printf("\n");
+	}
+	p = NULL;
+}
+
+void print_mc(const COEFS coefs, LEN len){
+	COEF c = &*coefs;
+	for(int i =0 ; i < len; i++){
+		for(int j =0 ; j < len; j++){
+			printf("%f\t", *c);
+			c++;
+		}
+		printf("\n");
+	}
+	c = NULL;
+}
+
 PIXELS_API STATUS copy_pixel(PIXEL src, PIXEL dest){
 	*src = *dest;
 }
@@ -55,19 +81,43 @@ PIXELS_API COEFS ptoc(PIXELS pixels, LEN len){
 
 PIXELS_API STATUS dct_pixels(PIXELS pixels, COEFS coefs, LEN len){
 	LEN N = len*len;
-	COEFS tmps = ptoc(pixels, len);
-	COEF tmp = tmps;
 	COEF coef = coefs;
+	PIXEL pixel;
 	for (INDEX k = 0; k < N; k++){
+		pixel = pixels;
 		*coef = 0.0f;
-		tmp = tmps;
 		for (INDEX n = 0; n < N; n ++){
-			*coef += *tmp*cos(PI*(0.5+n)*k/N);
-			tmp++;
+			*coef += *pixel*cos(PI*(0.5+n)*k/N);
+			pixel++;
 		}	
 		coef++;
 	}
 }
 
 PIXELS_API STATUS idct_pixels(COEFS coefs, PIXELS pixels, LEN len){
+	LEN N = len*len;
+	COEFS tmps = new_coefs(len, len);
+	COEF tmp = tmps;
+	COEF coef;
+	for (INDEX k = 0; k < N; k++){
+		coef = coefs;
+		*tmp =  0.5*(*coef);
+		for (INDEX n = 1; n < N; n ++){
+			*tmp += *coef*cos(PI*(0.5+k)*n/N)*2/N;
+			coef++;
+		}	
+		tmp++;
+	}
+
+	printf("IDCT RESULT:\n");
+	print_mc(tmps, len);
+	tmp = tmps;
+	PIXEL pixel = pixels;
+	for (INDEX i = 0; i < N; i ++){
+		*(pixel+i) = *(tmp+i);
+	}
+
+	printf("\n-----------------------------\n");
+	printf("IDCT PIXEL RESULT:\n");
+	print_mp(pixels, len);
 }
