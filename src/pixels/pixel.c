d6950412e8aa8476aab14797ecb2bce83fe46a4d
phyxdown <wucc@Ctrip.com>
2015-07-09 13:28:59 +0800

    implement COEFS by struct

--- src/pixels/pixel.c
+++ src/pixels/pixel.c
@@ -8,16 +8,12 @@ const double PI = 3.141592653589793238462643383279502884197169399375105820974944
 void print_mp(const PIXELS pixels, LEN len){ PIXEL p = &*pixels; for(int i =0 ; i < len; i++){ for(int j =0 ; j < len; j++){ unsigned char* bp = (unsigned char*)p; printf("(%d,%d,%d,%d) ", *bp, *(bp+1), *(bp+2), *(bp+3)); bp = NULL; p++; } printf("\n"); } p = NULL; } 
 void print_mc(const COEFS coefs, LEN len){ COEF c = &*coefs; for(int i =0 ; i < len; i++){ for(int j =0 ; j < len; j++){ printf("%8f ", *c); c++; } printf("\n"); } c = NULL; }
 
-PIXELS_API STATUS copy_pixel(PIXEL src, PIXEL dest){
-	*src = *dest;
-}
-
 PIXELS_API PIXELS new_pixels(LEN width, LEN height) {
-	return (PIXELS)calloc(width*height, sizeof(PIXELS));
+	return (PIXELS)calloc(width*height, sizeof(pixels_pixel_t));
 }
 
 PIXELS_API COEFS new_coefs(LEN width, LEN height) {
-	return (COEFS)calloc(width*height, sizeof(COEFS));
+	return (COEFS)calloc(width*height, sizeof(pixels_coef_t));
 }
 
 PIXELS_API STATUS scramble_pixels(PIXELS origin, PIXELS dest, LEN len, int a, int b){
@@ -46,29 +42,48 @@ PIXELS_API STATUS rescramble_pixels(PIXELS origin, PIXELS dest, LEN len, int a, 
 	}
 }
 
-PIXELS_API COEFS ptoc(PIXELS pixels, LEN len){
+PIXELS_API STATUS ptoc(PIXELS pixels, COEFS coefs, LEN len){
 	PIXEL pixel = pixels;
-	COEFS coefs = new_coefs(len, len);
 	COEF coef = coefs;
 	for (INDEX i = 0; i < len*len; i++){
-		*(coef+i) = *(pixel+i);
+		IMAGEDATA data = (IMAGEDATA)(pixel+i);
+		(*(coef+i)).R = *(data+0);
+		(*(coef+i)).G = *(data+1);
+		(*(coef+i)).B = *(data+2);
+		(*(coef+i)).A = *(data+3);
 	}
-	return coefs;
+} 
+
+PIXELS_API STATUS ctop(COEFS coefs, PIXELS pixels, LEN len){
+	COEF coef = coefs;
+	PIXEL pixel = pixels;
+	for (INDEX i = 0; i < len*len; i++){
+		IMAGEDATA data = (IMAGEDATA)(pixel+i);
+		*(data+0) = round((*(coef+i)).R);
+		*(data+1) = round((*(coef+i)).G);
+		*(data+2) = round((*(coef+i)).B);
+		*(data+3) = round((*(coef+i)).A);
+	}
 }
 
 PIXELS_API STATUS dct_pixels(PIXELS pixels, COEFS coefs, LEN len){
-	COEFS c_pixels = ptoc(pixels, len);
+	COEFS c_pixels = new_coefs(len, len);
+	ptoc(pixels, c_pixels, len);
 	LEN N = len*len;
 	COEF coef = coefs;
-	COEF pixel;
+	COEF c_pixel;
 	for (INDEX k = 0; k < N; k++){
-		pixel = c_pixels;
-		*coef = 0.0f;
+		c_pixel = c_pixels;
+		(*coef).R = 0.0f;
+		(*coef).G = (*(c_pixels+k)).G;
+		(*coef).B = (*(c_pixels+k)).B;
+		(*coef).A = (*(c_pixels+k)).A;
+
 		for (INDEX n = 0; n < N; n ++){
-			*coef += *pixel*cos(PI*(0.5+n)*k/N);
-			pixel++;
+			(*coef).R += (*c_pixel).R*cos(PI*(0.5+n)*k/N);
+			c_pixel++;
 		}	
-		*coef = *coef*2/N;
+		(*coef).R = (*coef).R*2/N;
 		coef++;
 	}
 }
@@ -80,17 +95,17 @@ PIXELS_API STATUS idct_pixels(COEFS coefs, PIXELS pixels, LEN len){
 	COEF coef;
 	for (INDEX k = 0; k < N; k++){
 		coef = coefs;
-		*tmp =  0.5*(*coef);
+		(*tmp).R =  0.5*(*coef).R;
+		(*tmp).G = (*(coef+k)).G;
+		(*tmp).B = (*(coef+k)).B;
+		(*tmp).A = (*(coef+k)).A;
 		coef ++;
 		for (INDEX n = 1; n < N; n ++){
-			*tmp += *coef*cos(PI*(0.5+k)*n/N);
+			(*tmp).R += (*coef).R*cos(PI*(0.5+k)*n/N);
 			coef++;
 		}	
 		tmp++;
 	}
 	tmp = tmps;
-	PIXEL pixel = pixels;
-	for (INDEX i = 0; i < N; i ++){
-		*(pixel+i) = *(tmp+i);
-	}
+	ctop(tmps, pixels, len);
 }
--- src/pixels/pixel.h
+++ src/pixels/pixel.h
@@ -17,8 +17,8 @@ struct __pixels_coef {
 	double A;
 };
 typedef struct __pixels_coef pixels_coef_t;
-typedef double* COEFS;
-typedef double* COEF;
+typedef pixels_coef_t* COEFS;
+typedef pixels_coef_t* COEF;
 
 typedef int X;
 typedef int Y;
@@ -26,12 +26,15 @@ typedef int INDEX;
 
 typedef unsigned long LEN; 
 
-PIXELS_API STATUS copy_pixel(PIXEL src, PIXEL dest);
 PIXELS_API PIXELS new_pixels(LEN width, LEN height);
 PIXELS_API COEFS new_coefs(LEN width, LEN height);
+
 PIXELS_API STATUS scramble_pixels(PIXELS origin, PIXELS dest, LEN len, int a, int b);
 PIXELS_API STATUS rescramble_pixels(PIXELS origin, PIXELS dest, LEN len, int a, int b);
-PIXELS_API COEFS ptoc(PIXELS pixels, LEN len);
+
+PIXELS_API STATUS ptoc(PIXELS pixels, COEFS coefs, LEN len);
+PIXELS_API STATUS ctop(COEFS coefs, PIXELS pixels, LEN len);
+
 PIXELS_API STATUS dct_pixels(PIXELS pixels, COEFS coefs, LEN len);
 PIXELS_API STATUS idct_pixels(COEFS coefs, PIXELS pixels, LEN len);
 
