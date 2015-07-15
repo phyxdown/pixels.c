86ccac4b17b263b2a97e6c35e9d71f9c5a241a1b
phyxdown <wucc@Ctrip.com>
2015-07-15 13:17:07 +0800

    round255

--- src/pixels/math.c
+++ src/pixels/math.c
@@ -11,3 +11,10 @@ int mod(long dividend, long divider) {
 double round(double origin){
 	return floor(origin+0.5);
 }
+
+double round255(double origin){
+	double ret = floor(origin+0.5);
+	if (ret > 255) return 255;
+	if (ret < 0 ) return 0;
+	return floor(origin+0.5);
+}
--- src/pixels/math.h
+++ src/pixels/math.h
@@ -2,3 +2,4 @@
 int mod(long dividend, long divider);
 
 double round(double origin);
+double round255(double origin);
